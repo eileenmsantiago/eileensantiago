2af80c9724bcabcd050dbfb1c5ddff4211bc5dd3
Eileen Santiago <eileenmvs@gmail.com>
2020-04-26 20:18:50 -0400

    full app

--- server/lambda.js
+++ server/lambda.js
@@ -1,6 +1,5 @@
 const serverless = require("serverless-http");
 const app = require("./app.js");
 
-module.exports = {
-    handler: serverless(app)
-};
+module.exports = app;
+module.exports.handler = serverless(app);
