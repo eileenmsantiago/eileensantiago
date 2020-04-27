98a0ad5aba28f846734a91d9efa4d8317d750a92
Eileen Santiago <eileenmvs@gmail.com>
2020-04-26 20:03:48 -0400

    add serverless-http package

--- functions/server.js
+++ functions/server.js
@@ -1,5 +1,6 @@
 const serverless = require("serverless-http");
-const app = require("../server/app.js");
+const app = require("../server/app");
 
-module.exports = app;
-module.exports.handler = serverless(app);
+module.exports = {
+    handler: serverless(app)
+};
