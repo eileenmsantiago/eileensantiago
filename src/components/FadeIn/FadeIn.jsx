90f79d1819a9f3433aa02debc8c3c3a688b6422d
Eileen Santiago <eileenmvs@gmail.com>
2020-05-26 21:22:31 -0400

    clean up work

--- src/components/FadeIn/FadeIn.jsx
+++ src/components/FadeIn/FadeIn.jsx
@@ -1,15 +1,24 @@
 import React , { useState, useEffect, useRef } from 'react';
 
 function FadeIn(props) {
-    const [ isVisible, setVisible ] = useState(true);
+    
+    const [ isVisible, setVisible ] = useState(false);
     const domRef = useRef();
+
     useEffect(() => {
+
         const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible(entry.isIntersecting));
-        });
+            entries.forEach(entry => {
+                if(!isVisible) {
+                    setVisible(entry.isIntersecting)
+                }
+            });
+        })
         observer.observe(domRef.current);
+        
         return () => observer.unobserve(domRef.current);
-    }, []);
+    }, [isVisible]);
+
     return (
         <div
             className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -11,56 +11,58 @@ const Work = (props) => {
     const {projects} = props;
     return(
         <>
-            <Hero 
-                pageTitle="A Portfolio" 
-                heading="by Eileen"
-                description="A systems thinker, based in Toronto, who uses design & code to strategize in creating digital product solutions"
-                showEmail = {true}
-            />
-            <Container>
-                <img src="/assets/scroll.svg" className="divider"/>
-                {
-                    projects.map((project, index) => {
+            <section className="hero">
+                <Container width="md">
+                    <Hero 
+                        pageTitle="A Portfolio" 
+                        heading="by Eileen"
+                        description="A systems thinker, based in Toronto, who uses design & code to strategize in creating digital product solutions"
+                        showEmail = {true}
+                    />
+                </Container>
+            </section>
+            <section className="projects">
+                <Container width="md">
+                    {projects.map((project, index) => {
                         return (
                             <FadeIn>
-
                                 <article className="projects">
-                                        <div className="row projects__container" style={{flexDirection: index % 2 == 1 ? 'row-reverse' : undefined}}>
-                                            <div className="col-md-6">
-                                                <a href={project.route} className="cta e-nav__link" >   
-                                                    <img className="projects__container-image" src={project.tileImg}/>
+                                    <div className="row projects__container" style={{flexDirection: index % 2 == 1 ? 'row-reverse' : undefined}}>
+                                        <div className="col-md-6">
+                                            <a href={project.route} className="cta" >   
+                                                <img className="projects__container-image" src={project.tileImg}/>
+                                            </a>
+                                        </div>
+                                        <div className="col-md-6 projects__container-content">
+                                            <div className="projects__container-content-wrapper">
+                                                <a href={project.route} className="cta " >   
+                                                    <Heading className="project__container--header" level="h3" style="h2">{project.title}</Heading>
                                                 </a>
-                                            </div>
-                                            <div className="col-md-6 projects__container-content">
-                                                <div className="projects__container-content-wrapper">
-                                                    <a href={project.route} className="cta e-nav__link" >   
-                                                        <Heading className="project__container--header" level="h3" style="h2">{project.title}</Heading>
-                                                    </a>
-                                                    <Paragraph size="reg" weight="regular" color="grey">
-                                                        {project.description}
+                                                <Paragraph size="reg" weight="regular" color="black">
+                                                    {project.description}
+                                                </Paragraph>
+                                                <div className="description">
+                                                    <Paragraph className="tags" size="sm" weight="regular" color="grey">
+                                                        {project.tags.map(tag => (
+                                                            <>
+                                                                {tag} 
+                                                            </>
+                                                        ))}
                                                     </Paragraph>
-                                                    <div className="description">
-                                                        <Paragraph className="tags" size="sm" weight="bold" color="grey">
-                                                            {project.tags.map(tag => (
-                                                                <>
-                                                                    {tag} 
-                                                                </>
-                                                            ))}
-                                                        </Paragraph>
-                                                        <a href={project.route} className="cta e-nav__link" >   
-                                                            <Paragraph size="sm" weight="md" weight="bold" color="black">View More</Paragraph>
-                                                            <img className="cta-icon" src="/assets/arrow.svg"/>
-                                                        </a>
-                                                    </div>
+                                                    <a href={project.route} className="cta " >   
+                                                        <Paragraph size="sm" weight="md" weight="bold" color="black">View More</Paragraph>
+                                                        <img className="cta-icon" src="/assets/arrow.svg"/>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
                                 </article>
                             </FadeIn>
                         );
-                    })
-                }
-            </Container>
+                    })}
+                </Container>
+            </section>
         </> 
     );
 }
