9afec4bbc83cee019027570158697343e06d271e
Eileen Santiago <eileenmvs@gmail.com>
2021-09-29 10:17:22 -0400

    Update content

--- data/about.js
+++ data/about.js
@@ -1,48 +1,49 @@
 export default {
-  title: ''
+  title: "",
 }
 
 export const skills = [
-  'Pen + paper',
-  'Product + design thinking',
-  'Wireframing',
-  'Prototyping',
-  'Visual Design',
-  'User flows',
-  'Design system',
-  'Brand & identity',
-  'Design Systems',
-  'Front-end Development'
+  "Pen + paper",
+  "Product + design thinking",
+  "Wireframing",
+  "Prototyping",
+  "Visual Design",
+  "User flows",
+  "Design system",
+  "Brand & identity",
+  "Design Systems",
+  "Front-end Development",
 ]
 
 export const facts = [
-  'I studied Nursing at York University and completed a post-graduate in Interactive Media Management where I furthered my skills in UI/UX design and web development.',
-  'From nursing to design and development, helping people has always been a core value in my education and past experiences, and so pivoting to UI/UX design made sense to continue practicing user-centered design. Creating design systems became a passion of mine, where I can solve technical and design problems, both creatively and efficiently.',
-  'Hobbies I enjoy doing are reading on my Kindle, learning new recipes, going camping with my family, taking snapshot of micro moments in my day-to-day, and staying active and well-hydrated.',
+  "I studied nursing at York University, where I happenily to have admired digital designing and decided to transition into UX design. This is where I completed a post-graduate in Interactive Media Management, and furthered my skills in UI/UX design and also web development.",
+  "From nursing to design and development, helping people has always been a core value in my education and past experiences, and so pivoting to UX design made sense to continue practicing a user-centered approach to my work, in design. Creating design systems became a passion of mine, where I can solve technical and design problems, both creatively and efficiently.",
+  "I enjoy spending my time in the design community, and participating in sessions with designer colleagues, to continue learning more about design, business, and technology.",
+  "Asides from all of the design-talk, I enjoy reading on my Kindle, learning new recipes, going camping with my family, taking snapshot of micro moments in my day-to-day, and staying active and being well-hydrated.",
 ]
 
 export const values = [
-  '☺️ Honesty and kindness',
-  '🤝 Cooperativeness',
-  '💬 Communication',
-  '🌱 Eagerness to learn and grow'
+  "☺️ Honesty and kindness",
+  "🤝 Cooperativeness",
+  "💬 Communication",
+  "🌱 Eagerness to learn and grow",
 ]
 
 export const imageCaption = [
   {
-    imagePath:'/assets/images/summer2021-0.jpg',
-    caption: 'moved downtown, Toronto'
+    imagePath: "/assets/images/summer2021-0.jpg",
+    caption: "moved downtown, Toronto",
   },
   {
-    imagePath:'/assets/images/summer2021-1.jpg',
-    caption: 'learning new recipes',
-  }, 
-  {
-    imagePath: '/assets/images/summer2021-2.jpg',
-    caption:'staying hydrated',
+    imagePath: "/assets/images/summer2021-1.jpg",
+    caption: "learning new recipes",
   },
   {
-    imagePath: '/assets/images/summer2021-3.jpg',
-    caption:'taking a break, going camping',
-  }
+    imagePath: "/assets/images/summer2021-2.jpg",
+    caption: "staying hydrated",
+  },
+  {
+    imagePath: "/assets/images/summer2021-3.jpg",
+    caption: "taking a break, going camping",
+  },
 ]
