1453a169568482ec0f8454c4b8765098bf245ddc
Eileen Santiago <eileenmvs@gmail.com>
2021-09-29 15:02:36 -0400

    Update about

--- data/about.js
+++ data/about.js
@@ -16,9 +16,10 @@ export const skills = [
 ]
 
 export const facts = [
-  'I studied Nursing at York University and completed a post-graduate in Interactive Media Management where I furthered my skills in UI/UX design and web development.',
-  'From nursing to design and development, helping people has always been a core value in my education and past experiences, and so pivoting to UI/UX design made sense to continue practicing user-centered design. Creating design systems became a passion of mine, where I can solve technical and design problems, both creatively and efficiently.',
-  'Hobbies I enjoy doing are reading on my Kindle, learning new recipes, going camping with my family, taking snapshot of micro moments in my day-to-day, and staying active and well-hydrated.',
+  "I studied nursing at York University, where I happenily to have admired digital designing and decided to transition into UX design. This is where I completed a post-graduate in Interactive Media Management, and furthered my skills in UI/UX design and also web development.",
+  "From nursing to design and development, helping people has always been a core value in my education and past experiences, and so pivoting to UX design made sense to continue practicing a user-centered approach to my work, in design. Creating design systems became a passion of mine, where I can solve technical and design problems, both creatively and efficiently.",
+  "I enjoy spending my time in the design community, and participating in sessions with designer colleagues, to continue learning more about design, business, and technology.",
+  "Asides from all of the design-talk, I enjoy reading on my Kindle, learning new recipes, going camping with my family, taking snapshot of micro moments in my day-to-day, and staying active and being well-hydrated.",
 ]
 
 export const values = [
