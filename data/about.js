50369f681457604c8d8a48bd80955644faf3cd6f
Eileen Santiago <eileenmvs@gmail.com>
2021-10-01 15:26:23 -0400

    Update about copy

--- data/about.js
+++ data/about.js
@@ -16,7 +16,7 @@ export const skills = [
 ]
 
 export const facts = [
-  "I studied nursing at York University, where I happenily to have admired digital designing and decided to transition into UX design. This is where I completed a post-graduate in Interactive Media Management, and furthered my skills in UI/UX design and also web development.",
+  "I studied nursing at York University, where I happenily to have admired digital designing and decided to transition into UX design. I completed a post-graduate in Interactive Media Management, and furthered my skills in UI/UX design and also web development.",
   "From nursing to design and development, helping people has always been a core value in my education and past experiences, and so pivoting to UX design made sense to continue practicing a user-centered approach to my work, in design. Creating design systems became a passion of mine, where I can solve technical and design problems, both creatively and efficiently.",
   "I enjoy spending my time in the design community, and participating in sessions with designer colleagues, to continue learning more about design, business, and technology.",
   "Asides from all of the design-talk, I enjoy reading on my Kindle, learning new recipes, going camping with my family, taking snapshot of micro moments in my day-to-day, and staying active and being well-hydrated.",
